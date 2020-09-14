81552adc64b3cc6fb076b83525482deb27df3e4c
aregkarapetyan <38075172+aregkarapetyan@users.noreply.github.com>
2020-09-14 18:59:48 +0400

    Add files via upload

---
+++ bus_data.py
@@ -0,0 +1,108 @@
+#Created by Areg Karapetyan
+
+#All of the streets
+class Streets:
+    def __init__(self,name):
+        self.name = name
+
+#Creation of instances for each street of class Streets
+if 1:
+    Abovyan = Streets("Abovyan")
+    Admiral_Isakov = Streets("Admiral Isakov")
+    Aeracia = Streets("Aeracia")
+    Agatangeghos = Streets("Agatangeghos")
+    Amiryan = Streets("Amiryan")
+    Araratyan = Streets("Araratyan")
+    Arno_Babajanyan = Streets("Arno Babajanyan")
+    Arshakunyats = Streets("Arshakunyats")
+    Artashisyan = Streets("Artashisyan")
+    Artsakh = Streets("Artsakh")
+    Atoyan = Streets("Atoyan")
+    Avetisyan = Streets("Avetisyan")
+    Ayvazovskiy = Streets("Ayvazovskiy")
+    Azatutyan = Streets("Azatutyan")
+    Badal_Muradian = Streets("Badal Muradian")
+    Baghramyan = Streets("Baghramyan")
+    Bagratunyats = Streets("Bagratunyats")
+    Bakunts = Streets("Bakunts")
+    Belinsku = Streets("Belinsku")
+    Bashinjaghyan = Streets("Bashinjaghyan")
+    Charents = Streets("Charents")
+    Davit_Anhaght = Streets("Davit Anhaght")
+    Davit_Bek = Streets("Davit Bek")
+    Dro = Streets("Dro")
+    Erebuni = Streets("Erebuni")
+    Gai = Streets("Gai")
+    Gazprom = Streets("Gazprom")
+    Gyulikevkhyan = Streets("Gyulikevkhyan")
+    Gyurjyan = Streets("Gyurjyan")
+    Halabyan = Streets("Halabyan")
+    Hasratyan = Streets("Hasratyan")
+    Hovhannisyan = Streets("Hovhannisyan")
+    Hunan_Avetisyan = Streets("Hunan Avetisyan")
+    Iosifyan = Streets("Iosifyan")
+    Janibekyan = Streets("Janibekyan")
+    Jughai = Streets("Jughai")
+    Kasyan = Streets("Kasyan")
+    Khaghagh_Don = Streets("Khaghagh Don")
+    Khanjyan = Streets("Khanjyan")
+    Kievyan = Streets("Kievyan")
+    Kilikia = Streets("Kilikia")
+    Kochinyan = Streets("Kochinyan")
+    Komitas = Streets("Komitas")
+    Koryun = Streets("Koryun")
+    Leningradyan = Streets("Leningradyan")
+    Lepsius = Streets("Lepsius")
+    Margaryan = Streets("Margaryan")
+    Mashtots = Streets("Mashtots")
+    Moskovyan = Streets("Moskovyan")
+    Myasnikyan = Streets("Myasnikyan")
+    Nanseni = Streets("Nanseni")
+    Nerqin_Charbakh = Streets("Nerqin Charbakh")
+    Nersisyan = Streets("Nersisyan")
+    Nzhdehi = Streets("Nzhdehi")
+    Qanaqercu = Streets("Qanaqercu")
+    Radiotun = Streets("Radiotun")
+    Raffi = Streets("Raffi")
+    Republic_Square = Streets("Republic Square")
+    Rubinyants = Streets("Rubinyants")
+    Samvel_Safaryani = Streets("Samvel Safaryani")
+    Saralanj = Streets("Saralanj")
+    Sasna_Tzrer = Streets("Sasna Tzrer")
+    Sasuntsi_Davit = Streets("Sasuntsi Davit")
+    Sayat_Nova = Streets("Sayat-Nova")
+    Sebastia = Streets("Sebastia")
+    Sevak = Streets("Sevak")
+    Shiraki = Streets("Shiraki")
+    Shopron = Streets("Shopron")
+    Tbilisyan = Streets("Tbilisyan")
+    Tevosyan = Streets("Tevosyan")
+    Tigran_Mets = Streets("Tigran Mets")
+    Tigran_Petrosyan = Streets("Tigran Petrosyan")
+    Titogradyan = Streets("Titogradyan")
+    Tumanyan = Streets("Tumanyan")
+    Ulnetsi = Streets("Ulnetsi")
+    Vagharshyan = Streets("Vagharshyan")
+    Victory_Bridge = Streets("Victory Bridge")
+    Vilnius = Streets("Vilnius")
+    Yeghvard = Streets("Yeghvard")
+
+bus_str_0 = []
+bus_str_1 = [Khaghagh_Don.name,Titogradyan.name,Ayvazovskiy.name,Erebuni.name,Artsakh.name,Tigran_Mets.name,Republic_Square.name,Amiryan.name,Mashtots.name,Baghramyan.name,Kasyan.name,Komitas.name,Azatutyan.name,Sevak.name, Dro.name,Lepsius.name]
+bus_str_2 = []
+bus_str_3 = [Arno_Babajanyan.name,Raffi.name,Sebastia.name,Leningradyan.name,Kievyan.name,Kasyan.name,Komitas.name,Davit_Anhaght.name,Rubinyants.name,Gai.name,Badal_Muradian.name,Davit_Bek.name,Hunan_Avetisyan.name]
+bus_str_4 = []
+bus_str_5 = [Kochinyan.name,Tevosyan.name,Hovhannisyan.name,Bakunts.name,Gyulikevkhyan.name,Gai.name,Myasnikyan.name,Abovyan.name,Moskovyan.name,Mashtots.name,Victory_Bridge.name,Admiral_Isakov.name,Kilikia.name]
+bus_str_6 = []
+bus_str_7 = [Khaghagh_Don.name,Titogradyan.name,Ayvazovskiy.name,Erebuni.name,Atoyan.name,Belinsku.name,Sasuntsi_Davit.name,Tigran_Mets.name,Khanjyan.name,Tumanyan.name,Mashtots.name,Koryun.name,Abovyan.name,Myasnikyan.name,Saralanj.name,Avetisyan.name,Vagharshyan.name,Sasna_Tzrer.name,Tigran_Petrosyan.name,Yeghvard.name]
+bus_str_8 = [Tigran_Petrosyan.name,Iosifyan.name,Halabyan.name,Kievyan.name,Baghramyan.name,Moskovyan.name,Abovyan.name,Myasnikyan.name,Gai.name,Badal_Muradian.name,Davit_Bek.name,Avetisyan.name,Vilnius.name]
+bus_str_9 = []
+bus_str_10 = []
+bus_str_11 = []
+bus_str_12 = [Aeracia.name,Shiraki.name,Artashisyan.name,Bagratunyats.name,Sebastia.name,Hasratyan.name,Janibekyan.name,Bashinjaghyan.name,Margaryan.name,Halabyan.name,Iosifyan.name,Tigran_Petrosyan.name]
+bus_str_13 = [Gazprom.name,Tbilisyan.name,Qanaqercu.name,Azatutyan.name,Komitas.name,Kasyan.name,Baghramyan.name,Sayat_Nova.name,Charents.name,Radiotun.name]
+bus_str_14 = [Lepsius.name,Dro.name,Sevak.name,Ulnetsi.name,Nersisyan.name,Azatutyan.name,Saralanj.name,Abovyan.name,Moskovyan.name,Khanjyan.name,Tigran_Mets.name,Artsakh.name,Nzhdehi.name,Arshakunyats.name,Shiraki.name,Araratyan.name,Nerqin_Charbakh.name]
+bus_str_15 = [Badal_Muradian.name,Shopron.name,Gyurjyan.name,Jughai.name,Nanseni.name,Samvel_Safaryani.name,Gai.name,Myasnikyan.name,Abovyan.name,Moskovyan.name,Khanjyan.name,Agatangeghos.name,Arshakunyats.name,Nzhdehi.name,Bagratunyats.name,Artashisyan.name,Shiraki.name]
+
+bus_str = [bus_str_0,bus_str_1,bus_str_2,bus_str_3,bus_str_4,bus_str_5,bus_str_6,bus_str_7,bus_str_8,bus_str_9,bus_str_10,bus_str_11,bus_str_12,bus_str_13,bus_str_14,bus_str_15]
+
